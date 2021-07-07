7012d6c56746cb2eaaf79511af5411d0ec988ff1
Lwazi <slwazit@gmail.com>
2021-07-07 13:54:42 +0200

    Started with signal strength APP

--- app/src/main/java/com/example/signalstrength/MainActivity.java
+++ app/src/main/java/com/example/signalstrength/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.signalstrength;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TelephonyManager mTelephonyManager;
+    MyPhoneStateListener mPhoneStatelistener;
+    int mSignalStrength = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mPhoneStatelistener = new MyPhoneStateListener();
+        mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        mTelephonyManager.listen(mPhoneStatelistener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
     }
 }
